287de3ac5401a1f64078b17e602cd679681e7c19
Engr Unuefe Ejovwoke <66459477+unuefeejovwoke@users.noreply.github.com>
2023-12-15 20:22:14 +0100

    Update settings.py
    Changed port from 3000 to 4000

--- django_project/settings.py
+++ django_project/settings.py
@@ -90,11 +90,11 @@ MIDDLEWARE = [
 ]
 
 CORS_ORIGIN_WHITELIST = (
-    "http://localhost:3000",
+    "http://localhost:4000",
     "http://localhost:8000",
 )
 
-CSRF_TRUSTED_ORIGINS = ["http://localhost:3000"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost:4000"]
 
 ROOT_URLCONF = 'django_project.urls'
 
